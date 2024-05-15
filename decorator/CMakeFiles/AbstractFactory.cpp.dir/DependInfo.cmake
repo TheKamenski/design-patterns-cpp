f7e83e2eec78dedb13734066caa8ae66fb74eb19
TheKamenski <k.kamenskiy@gmail.com>
2024-05-15 16:33:40 +0300

    added files

---
+++ decorator/CMakeFiles/AbstractFactory.cpp.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/konstantin/design-patterns-cpp/abstract-factory/AbstractFactory.cpp" "CMakeFiles/AbstractFactory.cpp.dir/abstract-factory/AbstractFactory.cpp.o" "gcc" "CMakeFiles/AbstractFactory.cpp.dir/abstract-factory/AbstractFactory.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ decorator/CMakeFiles/AbstractFactory.cpp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AbstractFactory.cpp.
+# This may be replaced when dependencies are built.
